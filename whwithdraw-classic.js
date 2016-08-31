240641a69c6dd10809ac04fea49e7e68429e3d90
hsu.shufu <shufu41@gmail.com>
2016-08-31 12:06:08 +0800

    whitehat etc withdraw contract address update

--- whwithdraw-classic.js
+++ whwithdraw-classic.js
@@ -1,6 +1,6 @@
 // https://www.reddit.com/r/ethereum/comments/4yhz8h/etc_withdraw_contract_to_be_reviewed/
 // whitehat withdraw
-var whwd_addr = "0x180826b05452ce96e157f0708c43381fee64a6b8";
+var whwd_addr = "0x9f5304da62a5408416ea58a17a92611019bd5ce3";
 var whwd_abi = [{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[],"name":"seal","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"totalAccounts","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"data","type":"uint256[]"}],"name":"fill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"sealed","outputs":[{"name":"","type":"bool"}],"type":"function"},{"inputs":[],"type":"constructor"}]; 
 var whwd = web3.eth.contract(whwd_abi).at(whwd_addr); 
 
